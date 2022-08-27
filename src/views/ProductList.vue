7a22ab1496e349ccdf69a6916ccb291e16e643af
asooso12 <asooso1212@gmail.com>
2022-08-27 18:33:44 +0900

    0827 afternoon

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="product-list">
-    <ProductItemVue
-      v-for="(item, idx) in testListItem"
+    <SubmenuTabVue 
+
+    />
+    <ProductItemVue 
+      v-for="(item, idx) in testItemList"
       :key="idx"
       :product="item"
     />
-    </div>
+    
+  </div>
       
 </template>
 
 <script>
 import ProductItemVue from '@/components/ProductItem.vue';
+import SubmenuTabVue from '@/components/SubmenuTab.vue';
 export default {
   name: 'ProductList',
   components: {
     ProductItemVue,
+    SubmenuTabVue,
   },
   data() {
     return {
       testItemList: [{
+        category: '여름과일',
         // 제품명
         productName: '대부도/송산 캠벨포도',
         // 제품 가격
@@ -42,10 +49,35 @@ export default {
         commentCount: 41,
       },
       {
-
-      },]
+        category: '여름과일',
+        // 제품명
+        productName: '샤인머스캣',
+        // 제품 가격
+        price: 16900,
+        // 제품 이미지 path
+        productImg: require('@/assets/shine.jpeg'),
+        // 행사 목록
+        promotionList: [{
+          color: 'red',
+          promotionType: 'amount',
+          promotionValue: '3000',
+          promotionCondition: '신세계포인트 적립 시'
+        },{
+          // color&Type 정규화필요
+          color: 'blue',
+          promotionType: 'amount',
+          promotionValue: '3000',
+          promotionCondition: '1+1'
+        },],
+        likeCount: 161,
+        commentCount: 41,
+      },],
+      subMenus: [],
     }
-  }
+  },
+  methods: {
+    
+  },
 }
 </script>
 
